ab1f622b34d4def435530e6bd3567c7fb92c9ac0
karnotxo <jmanuelcostasantos@gmail.com>
2020-02-07 03:04:36 +0100

    Added tree-like bite component

--- airsrcc/airsrcc-client/src/main/java/com/indra/srcc/airsrcc/client/ui/bite/BiteDiagram.java
+++ airsrcc/airsrcc-client/src/main/java/com/indra/srcc/airsrcc/client/ui/bite/BiteDiagram.java
@@ -8,12 +8,16 @@ import java.util.Map;
 
 import javax.swing.JComponent;
 
+import com.mxgraph.layout.mxCompactTreeLayout;
 import com.mxgraph.layout.mxGraphLayout;
 import com.mxgraph.layout.mxParallelEdgeLayout;
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.layout.orthogonal.mxOrthogonalLayout;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.util.mxConstants;
+import com.mxgraph.util.mxEvent;
+import com.mxgraph.util.mxEventObject;
+import com.mxgraph.util.mxEventSource;
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxGraphView;
 import com.mxgraph.view.mxStylesheet;
@@ -23,8 +27,15 @@ public class BiteDiagram {
 	private mxGraphComponent graphComponent = null;
 
 	public BiteDiagram() {
-		mxGraph graph = new mxGraph();
-		Object parent = graph.getDefaultParent();
+		mxGraph graph = new ExpandableTreeGraph();
+
+        mxCompactTreeLayout layout = new mxCompactTreeLayout(graph, false);         
+        layout.setUseBoundingBox(false);
+        layout.setEdgeRouting(false);
+        layout.setLevelDistance(30);
+        layout.setNodeDistance(10);
+
+        Object parent = graph.getDefaultParent();
 
 		graph.getModel().beginUpdate();
 		try {
@@ -36,6 +47,8 @@ public class BiteDiagram {
 			Object v3 = graph.insertVertex(parent, null, "SSR", 150, 100, 80, 30, 
 					"fillColor=#ff0000;gradientColor=#ff0f0f;shadow=1");
 			graph.insertEdge(parent, null, null, v1, v3, "edgeStyle=elbowEdgeStyle;elbow=verical;orthogonal=1;");
+			
+			layout.execute(parent);  
 		} finally {
 			graph.getModel().endUpdate();
 		}
@@ -45,12 +58,12 @@ public class BiteDiagram {
 		graph.setCellsLocked(true);
 		graph.setAllowDanglingEdges(false);
 		
-	    Map<String, Object> edge = new HashMap<String, Object>();
+	    /*Map<String, Object> edge = new HashMap<String, Object>();
 	    edge.put(mxConstants.STYLE_ROUNDED, true);
 	    edge.put(mxConstants.STYLE_ORTHOGONAL, false);
 	    edge.put(mxConstants.STYLE_EDGE, "elbowEdgeStyle");
 	    edge.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_CONNECTOR);
-	    edge.put(mxConstants.STYLE_ENDARROW, mxConstants.NONE/*ARROW_CLASSIC*/);
+	    edge.put(mxConstants.STYLE_ENDARROW, mxConstants.NONE);
 	    edge.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_MIDDLE);
 	    edge.put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
 	    edge.put(mxConstants.STYLE_STROKECOLOR, "#000000"); // default is #6482B9
@@ -59,9 +72,19 @@ public class BiteDiagram {
 	    mxStylesheet edgeStyle = new mxStylesheet();
 	    
 	    edgeStyle.setDefaultEdgeStyle(edge);
-	    graph.setStylesheet(edgeStyle);
+	    graph.setStylesheet(edgeStyle);*/
 		
-		graphComponent = new mxGraphComponent(graph);
+	    
+	    graph.addListener(mxEvent.FOLD_CELLS,  new mxEventSource.mxIEventListener() {
+			
+			@Override
+			public void invoke(Object sender, mxEventObject evt) {
+                layout.execute(graph.getDefaultParent());
+			}
+		});
+        //mxGraphComponent graphComponent = new mxGraphComponent(graph);
+	    
+        graphComponent = new mxGraphComponent(graph);
 		graphComponent.getGraphControl().addMouseWheelListener(new MouseWheelListener() {			
 			
 		@Override
@@ -80,9 +103,9 @@ public class BiteDiagram {
 		int viewLen = (int)view.getGraphBounds().getWidth();
 		view.setScale((double)compLen/viewLen * view.getScale());*/
 		new mxOrthogonalLayout(graph).execute(graph.getDefaultParent());
-		mxGraphLayout layout = new mxHierarchicalLayout(graph); 
-        layout.setUseBoundingBox(false);
-        layout.execute(graph.getDefaultParent());
+		/*mxGraphLayout layout2 = new mxHierarchicalLayout(graph); 
+        layout2.setUseBoundingBox(false);
+        layout2.execute(graph.getDefaultParent());*/
 		//new mxParallelEdgeLayout(graph).execute(graph.getDefaultParent());
 	}
 
